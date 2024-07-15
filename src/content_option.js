e43bbd705462a07d36e617546de76922e6807309
tarekHaker <tarekbnzaied@gmail.com>
2024-07-15 12:29:01 -0400

    update

---
+++ src/content_option.js
@@ -0,0 +1,171 @@
+const logotext = "Tarek Ben Zaied";
+const meta = {
+    title: "Tarek Ben Zaied",
+    description: "I’m Tarek Ben Zaied, I’m Full stack devloper",
+};
+
+const introdata = {
+    title: "Hi ! I’m Tarek Ben Zaied",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am a skilled Full Stack Web Developer with expertise in React, CSS, HTML, JavaScript, jQuery, MongoDB, MySQL, and PostgreSQL. I specialize in creating dynamic and responsive web applications, leveraging my extensive project experience.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "Mysql",
+        value: 80,
+    },
+    {
+        name: "MongoDB",
+        value: 85,
+    },
+    {
+        name: "NodeJS",
+        value: 80,
+    },
+    {
+        name: "PostgreSQL",
+        value: 70,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "With a keen eye for detail and a strong sense of aesthetics, I excel in UI & UX Design. I ensure that applications are not only functional but also intuitive, user-friendly, and visually appealing.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "In the realm of Mobile Apps, I develop responsive and high-performing applications for various platforms. I focus on creating seamless user experiences and robust functionality to meet the needs of modern mobile users.",
+    }
+   
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "tarekbnzaied@gmail.com",
+    YOUR_FONE: "+13435536883",
+    description: "If you have any questions, collaboration ideas, or opportunities you'd like to discuss, please feel free to reach out. I am always open to connecting with fellow professionals and exploring new ventures. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_ks91nrh",
+    YOUR_TEMPLATE_ID: "template_5xn422e",
+    YOUR_USER_ID: "YOUHbG-9KWCNZBUBC",
+};
+
+const socialprofils = {
+    github: "https://github.com/tarekHaker",
+    facebook: "https://www.facebook.com/tarek.benzaied",
+    linkedin: "https://www.linkedin.com/in/tarek-ben-zaied-708a2a1b3/",
+    twitter: "https://x.com/Tarekbza",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
